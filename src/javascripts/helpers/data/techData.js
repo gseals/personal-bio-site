fca3ea92298d7962f4dd7c490d90f63849699aea
Gabriel Seals <sealsgj@gmail.com>
2019-12-23 20:11:56 -0600

    axios call

--- src/javascripts/helpers/data/techData.js
+++ src/javascripts/helpers/data/techData.js
@@ -3,3 +3,18 @@ import apiKeys from '../apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
+const getTechData = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/technologies.json`)
+    .then((response) => {
+      const demTech = response.data;
+      const tech = [];
+      Object.keys(demTEch).forEach((fbId) => {
+        demTech[fbId].id = fbId;
+        tech.push(demTech[fbId]);
+      });
+      resolve(tech);
+    })
+    .catch((error) => reject(error));
+});
+
+export default { getTechData };
