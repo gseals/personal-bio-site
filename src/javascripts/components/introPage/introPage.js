7742ca3d36278e817b25e16894a5a47f738389d0
Gabriel Seals <sealsgj@gmail.com>
2019-12-24 16:18:12 -0600

    modifying text

--- src/javascripts/components/introPage/introPage.js
+++ src/javascripts/components/introPage/introPage.js
@@ -5,6 +5,8 @@ const introPage = () => {
   let domString = '';
   domString = `
   <h1 class="introText">Gabriel Seals</h1>
+  <div class="underline"></div>
+  <h2 class="introText">Software and Web Developer | Nashville Tennessee</h2>
   `;
   util.printToDom('introPage', domString);
 };
