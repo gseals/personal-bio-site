2cd235fb587b39d36a376c1b02dbfaf836be4b7b
Gabriel Seals <sealsgj@gmail.com>
2020-01-03 16:45:25 -0600

    updating styles

--- src/javascripts/components/biography/biography.js
+++ src/javascripts/components/biography/biography.js
@@ -5,19 +5,50 @@ import photo from '../../../assets/profilePic.jpg';
 const bioString = () => {
   let domString = '';
   domString = `
+  <div>
+  <h3 class="name">Hi! I'm Gabe</h3>
+  </div>
   <div class="row">
-  <div class="col-6">
+  <div class="col-4">
   <img id="bioImg" src="${photo}"/>
   </div>
-  <div class="col-6">
-  <div class="bioBackground">
-  <p class="bioParaText">
-      I grew up in a town of roughly 10,000 people. We raised chickens and drove four-wheelers. I decided to go to college because it was what everyong
-      else was doing and I didn't stop to question the path: high school, college, job, family, kids, etc. College challenged me and allowed me to meet a
-      lot of great people, but I did not put in the amount of time or dedication asked of me so I coasted. It wasn't until I really began considering my place,
-      effect on, and usefulness in the universe that I actually started putting effort in. I attended grad school both as a way to further my education and as a way
-      to prove I could do the work, contribute interesting ideas, provide effective feedback, and work towards a goal. I bring that mindset into my time at NSS.
-    </p>
+  <div class="col-8">
+  <div class="bioBackground bioParaText">
+  <p>
+  I graduated Tennessee Technological University with a degree in English Literature,
+  married the sweetest girl I’d ever met, and swiftly moved to Nashville.
+  </p>
+  <p>
+  While my wife pursued her doctorate, I pursued my Masters in
+  English through Belmont University. I attended grad school both
+  as a way to further my education and as a way to prove I could do
+  the work, contribute interesting ideas, provide effective feedback,
+  and work towards a goal. My wife and I graduated in 2017.
+  </p>
+  <p>
+  The road from there to here looks like this: I did some editing for a
+  literary magazine on the West Coast, some poetry judging for local contests,
+  assisted professors in grading essays, and published poems in literary journals
+  like <a hhref="http://www.boaatpress.com/coronation">BOAAT</a>, Frontier Poetry, and Rockvale Review.
+  </p>
+  <p>
+  In late 2018, I began to seriously consider how my time should be spent:
+  should I continue waiting for opportunities to become available, or should
+  I seek an opportunity and pursue it with the intention of spending the rest
+  of my life doing it? Luckily, I had some experience with coding as a graduate
+  school class required us to build out basic HTML and CSS websites. As the
+  Office Administrator for Belmont University’s campus clinic, I serve as the
+  point person for changes in our EHR - Medicat - while also wielding control
+  over our sector of Hannon Hill’s Cascade Content Management System.
+  </p>
+  <p>
+  The thing that excites me most about development rests on the idea that
+  an answer exists even though it may take time to locate it. The satisfaction
+  comes from figuring things out and, while figuring things out takes time, the
+  amount of time invested in finding the solution yields both results and a growing
+  knowledge-base that can be called upon to complete future tasks. In essence, time
+  in equals time out and then some</p>
+  </p>
   </div>
   </div>
   </div>
