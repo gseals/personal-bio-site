fec7d73d6b37799f0e7ff95be66ba2052c28c68e
Gabriel Seals <sealsgj@gmail.com>
2020-02-22 17:39:15 -0600

    crafting new project cards

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -8,18 +8,24 @@ const createProjectCards = () => {
     .then((projects) => {
       projects.forEach((project) => {
         domString += `
-        <div class="card projectCard mb-3" style="width: 18rem;">
-          <img src="${project.screenshot}" class="card-img-top" alt="${project.title}">
-          <div class="card-body">
-          <h5 class="card-title">${project.title}</h5>
-          <p class="card-text">${project.description}</p>
-          <p class="card-text">Created with: ${project.technologiesUsed}</p>
-          <div class="text-center">
-          <a href="${project.url}" target="_blank">Demo Site</a>
-          <a href="${project.githubUrl}" target="_blank">GitHub code</a>
-          </div>
-          </div>
-        </div>`;
+          <div class="card projectCard mb-3">
+            <div class="row no-gutters">
+              <div class="col-md-4">
+                <img src="${project.screenshot}" class="card-img-top" alt="${project.title}">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${project.title}</h5>
+                  <p class="card-text">${project.description}</p>
+                  <p class="card-text d-inline">Created with: ${project.technologiesUsed}</p>
+                  <div class="d-flex justify-content-between">
+                  <a href="${project.url}" target="_blank role="button" class="btn btn-primary">Demo Site</a>
+                  <a href="${project.githubUrl}" target="_blank" role="button" class="btn btn-primary">GitHub code</a>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
         util.printToDom('projectsDisplay', domString);
       })
         .catch((error) => console.error(error));
